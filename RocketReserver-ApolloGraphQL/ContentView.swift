907540d95ca51cd9f0ac20a6065ec67f0212612c
Negin Zahedi <zahedi.negin@gmail.com>
2024-01-31 13:15:31 -0500

    Initial Commit

---
+++ RocketReserver-ApolloGraphQL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RocketReserver-ApolloGraphQL
+//
+//  Created by Negin Zahedi on 2024-01-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RocketReserver-ApolloGraphQL/RocketReserver_ApolloGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketReserver_ApolloGraphQLApp.swift
+//  RocketReserver-ApolloGraphQL
+//
+//  Created by Negin Zahedi on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct RocketReserver_ApolloGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
