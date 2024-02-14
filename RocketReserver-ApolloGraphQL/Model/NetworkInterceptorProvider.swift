410425a46f2d1364a77242c57f19cf49be7cfb62
Negin Zahedi <zahedi.negin@gmail.com>
2024-02-14 14:10:35 -0500

    add  login authorization token to the operations

---
+++ RocketReserver-ApolloGraphQL/Model/NetworkInterceptorProvider.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkInterceptorProvider.swift
+//  RocketReserver-ApolloGraphQL
+//
+//  Created by Negin Zahedi on 2024-02-14.
+//
+
+import Foundation
+import Apollo
+import ApolloAPI
+
+class NetworkInterceptorProvider: DefaultInterceptorProvider {
+    
+    override func interceptors<Operation>(for operation: Operation) -> [ApolloInterceptor] where Operation : GraphQLOperation {
+        var interceptors = super.interceptors(for: operation)
+        interceptors.insert(AuthorizationInterceptor(), at: 0)
+        return interceptors
+    }
+    
+}
